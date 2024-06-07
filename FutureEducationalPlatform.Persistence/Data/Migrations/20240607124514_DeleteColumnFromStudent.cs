ef74c4962566d21ef67cd250d16cf93dac248fd5
mozam <mozamezo638@gmail.com>
2024-06-07 15:47:00 +0300

    migrationsAdded

---
+++ FutureEducationalPlatform.Persistence/Data/Migrations/20240607124514_DeleteColumnFromStudent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FutureEducationalPlatform.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteColumnFromStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentPhoneNumber",
+                table: "Students");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParentPhoneNumber",
+                table: "Students",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
