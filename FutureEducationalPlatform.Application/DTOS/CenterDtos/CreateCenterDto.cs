f5b4c8272254dcff7c416998c7fb7ae65120aeef
Alaa1554 <alakhshgv@gmail.com>
2024-06-17 22:11:20 +0300

    feat: Add CenterDtos

---
+++ FutureEducationalPlatform.Application/DTOS/CenterDtos/CreateCenterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureEducationalPlatform.Application.DTOS.CenterDtos
+{
+    public record CreateCenterDto : BaseCenterDto
+    {
+        public CreateCenterDto(string name, string address) : base(name, address) { }
+    }
+}
---
+++ FutureEducationalPlatform.Application/DTOS/CenterDtos/GetCenterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureEducationalPlatform.Application.DTOS.CenterDtos
+{
+    public record GetCenterDto:BaseCenterDto
+    {
+        public Guid Id { get; set; }
+        public GetCenterDto(string name,string address):base(name,address) { }
+    }
+}
