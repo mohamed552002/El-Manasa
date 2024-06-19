b1c672d7c7fd4589b6c6710ecca7a9a79c431141
mohamed552002 <mozamezo638@gmail.com>
2024-06-20 00:04:43 +0300

    Refactor:CenterCourseTimeValidator

--- FutureEducationalPlatform.Application/DTOS/CenterCourseTimeDtos/AddCenterCourseTimeDto.cs
+++ FutureEducationalPlatform.Application/DTOS/CenterCourseTimeDtos/AddCenterCourseTimeDto.cs
@@ -1,11 +1,11 @@
 ﻿
+
 namespace FutureEducationalPlatform.Application.DTOS.CenterCourseTimeDtos
 {
-    public record AddCenterCourseTimeDto
-    (
-        Guid CenterId,
-        Guid CourseId,
-        DayOfWeek LectureDay,
-        TimeSpan LectureTime
-    );
+    public record AddCenterCourseTimeDto : BaseCenterCourseTimeDto
+    {
+        public AddCenterCourseTimeDto(Guid CenterId, Guid CourseId, DayOfWeek LectureDay, TimeSpan LectureTime) : base(CenterId, CourseId, LectureDay, LectureTime)
+        {
+        }
+    }
 }
