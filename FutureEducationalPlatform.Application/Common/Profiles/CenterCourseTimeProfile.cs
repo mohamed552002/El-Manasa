cd065782de72c583d0b16f46a4a85683db4a3a7d
mohamed552002 <mozamezo638@gmail.com>
2024-06-19 18:54:16 +0300

    AddCourseCenterTimeDtos

---
+++ FutureEducationalPlatform.Application/Common/Profiles/CenterCourseTimeProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FutureEducationalPlatform.Application.DTOS.CenterCourseTimeDtos;
+using FutureEducationalPlatform.Domain.Entities.CenterEntites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureEducationalPlatform.Application.Common.Profiles
+{
+    public class CenterCourseTimeProfile : Profile
+    {
+        public CenterCourseTimeProfile()
+        {
+            CreateMap<AddCenterCourseTimeDto, CenterCourseTime>();
+            CreateMap<UpdateCenterCourceDto, CenterCourseTime>();
+            CreateMap<CenterCourseTime, GentCenterCourseTimeDto>();
+        }
+    }
+}
---
+++ FutureEducationalPlatform.Application/DTOS/CenterCourseTimeDtos/UpdateCenterCourceDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureEducationalPlatform.Application.DTOS.CenterCourseTimeDtos
+{
+    public record UpdateCenterCourceDto
+    (
+        Guid CenterId,
+        Guid CourseId,
+        DayOfWeek LectureDay,
+        TimeSpan LectureTime
+    );
+
+}
