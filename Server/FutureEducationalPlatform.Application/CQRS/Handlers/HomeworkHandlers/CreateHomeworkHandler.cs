ddfdef189c73e126fac1177a44a55f204e33e13e
mohamed552002 <mozamezo638@gmail.com>
2024-07-06 23:18:51 +0300

    AddHomewrokImplementationAndDtos

---
+++ Server/FutureEducationalPlatform.Application/CQRS/Handlers/HomeworkHandlers/CreateHomeworkHandler.cs
@@ -0,0 +1,26 @@
+﻿using FutureEducationalPlatform.Application.CQRS.Commands.HomeworkCommands;
+using FutureEducationalPlatform.Application.DTOS.HomeworkDtos;
+using FutureEducationalPlatform.Application.Interfaces.IServices;
+using FutureEducationalPlatform.Domain.Entities.HomeworkEntites;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureEducationalPlatform.Application.CQRS.Handlers.HomeworkHandlers
+{
+    public class CreateHomeworkHandler : BaseHomeworkHandler, IRequestHandler<CreateHomeworkRequest, string>
+    {
+        public CreateHomeworkHandler(IBaseService<Homework, GetHomeworkDto, CreateHomeworkDto, UpdateHomeworkDto> baseService) : base(baseService)
+        {
+        }
+
+        public async Task<string> Handle(CreateHomeworkRequest request, CancellationToken cancellationToken)
+        {
+            await _baseService.CreateAsync(request.CreateHomeworkDto);
+            return "تمت اضافة الواجب بنجاح";
+        }
+    }
+}
---
+++ Server/FutureEducationalPlatform.Persistence/Data/Migrations/20240706192748_EditHomeworkTableAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FutureEducationalPlatform.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditHomeworkTableAttribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActived",
+                table: "Homeworks",
+                newName: "IsActive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Homeworks",
+                newName: "IsActived");
+        }
+    }
+}
