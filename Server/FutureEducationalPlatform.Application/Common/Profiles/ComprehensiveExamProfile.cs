4e0f044538a2cf2c824a31fb8d18ed9fea0c6c98
mohamed552002 <mozamezo638@gmail.com>
2024-07-16 04:29:59 +0300

    Refactor:ComprehensiveExam Cleaning Code

--- Server/FutureEducationalPlatform.Application/Common/Profiles/ComprehensiveExamProfile.cs
+++ Server/FutureEducationalPlatform.Application/Common/Profiles/ComprehensiveExamProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using FutureEducationalPlatform.Application.DTOS.ComprehensiveExamDtos;
 using FutureEducationalPlatform.Domain.Entities.ComprehensiveExamEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FutureEducationalPlatform.Application.Common.Profiles
 {
--- Server/FutureEducationalPlatform.Persistence/EntityConfiguration/ComprehensiveExamEntities/ComprehensiveExamConfiguration.cs
+++ Server/FutureEducationalPlatform.Persistence/EntityConfiguration/ComprehensiveExamEntities/ComprehensiveExamConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using FutureEducationalPlatform.Domain.Entities.ComprehensiveExamEntities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FutureEducationalPlatform.Persistence.EntityConfiguration.ComprehensiveExamEntities
 {
